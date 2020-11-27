7ec72a26320348701d02b5a4e77870f8d2f5ddec
x-zho14ls <18813119938@163.com>
2020-11-27 20:00:15 +0800

    Update conv_type.py

--- utils/conv_type.py
+++ utils/conv_type.py
@@ -127,6 +127,45 @@ class ProbMaskConvChannelDiscrete(nn.Conv2d):
         return x
 
 
+class ProbMaskConvChannelDiscreteSpeedUp(nn.Conv2d):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.scores = nn.Parameter(torch.Tensor(self.weight.size()[0], 1, 1, 1))
+        self.subnet = None
+        self.train_weights = False
+        self.rescaling_para = nn.Parameter(torch.Tensor(1))
+        if parser_args.score_init_constant is not None:
+            self.scores.data = (
+                    torch.ones_like(self.scores) * parser_args.score_init_constant
+            )
+        else:
+            nn.init.kaiming_uniform_(self.scores, a=math.sqrt(5))
+
+    @property
+    def clamped_scores(self):
+        return self.scores
+
+    def fix_subnet(self):
+        self.subnet = (torch.rand_like(self.scores) < self.clamped_scores).float()
+
+    def forward(self, x):
+        if not self.train_weights:
+            eps = 1e-20
+            temp = parser_args.T
+            uniform0 = torch.rand_like(self.scores)
+            uniform1 = torch.rand_like(self.scores)
+            noise = -torch.log(torch.log(uniform0 + eps) / torch.log(uniform1 + eps) + eps)
+            self.subnet = GetMaskDiscrete.apply(torch.sigmoid((torch.log(self.clamped_scores + eps) - torch.log(1.0 - self.clamped_scores + eps) + noise) * temp))
+            size = list(self.weight.size()[1:])
+            size.insert(0, self.subnet.sum())
+            w = torch.masked_select(self.weight, self.subnet).view(size)
+            x = F.conv2d(x, w, self.bias, self.stride, self.padding, self.dilation, self.groups)
+        else:
+            w = self.weight * self.subnet
+            x = F.conv2d(x, w, self.bias, self.stride, self.padding, self.dilation, self.groups)
+        return x
+
+
 class GetMaskDiscrete(autograd.Function):
     @staticmethod
     def forward(ctx, m_cont):
