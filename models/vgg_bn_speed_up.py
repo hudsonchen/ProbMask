1680f21e659787cf488704bc782eeba33034fd1e
x-zho14ls <18813119938@163.com>
2020-11-27 22:39:03 +0800

    a

--- models/vgg_bn_speed_up.py
+++ models/vgg_bn_speed_up.py
@@ -2,6 +2,7 @@ import torch.nn as nn
 import torch
 from utils.builder import get_builder
 from args import args as parser_args
+import timeit
 
 class VGG19(nn.Module):
 
@@ -49,64 +50,66 @@ class VGG19(nn.Module):
 
     def forward(self, x):
         # print("--1--")
-        x, mask = self.conv1(x)
+        # x, mask = self.conv1(x)
+        print(timeit.repeat(stmt="x, mask = self.conv1(x)", repeat=2, number=10000))
+
         # print("--2--")
         # print("x, mask", x.size(), mask.size())
-        x = self.bn1(x)
-        # print("x, after bn1", x.size())
-        # print("before select: ", self.bn1_w.size(), mask.size())
-        # print("after select: ", torch.masked_select(self.bn1_w, mask.squeeze()).size())
-        masked_bn1_w = torch.masked_select(self.bn1_w, mask.squeeze()).view(1, mask.sum(), 1, 1)
-        masked_bn1_b = torch.masked_select(self.bn1_b, mask.squeeze()).view(1, mask.sum(), 1, 1)
-        # print("m_w, m_b, x", masked_bn1_w.size(), masked_bn1_b.size(), x.size())
-        x = x*masked_bn1_w+masked_bn1_b
-        # print("m_w, m_b, x", masked_bn1_w.size(), masked_bn1_b.size(), x.size())
-        x = nn.ReLU(inplace=True)(x)
-        x, mask = self.conv2(x, mask)
-        # print("--3--")
-
-        x = nn.ReLU(inplace=True)(self.bn2(x))
-        x = nn.MaxPool2d(kernel_size=2, stride=2)(x)
-
-        x, mask = self.conv3(x, mask)
-        # print("--4--")
-
-        x = nn.ReLU(inplace=True)(self.bn3(x))
-        x, mask = self.conv4(x, mask)
-        x = nn.ReLU(inplace=True)(self.bn4(x))
-        x = nn.MaxPool2d(kernel_size=2, stride=2)(x)
-
-        x, mask = self.conv5(x, mask)
-        x = nn.ReLU(inplace=True)(self.bn5(x))
-        x, mask = self.conv6(x, mask)
-        x = nn.ReLU(inplace=True)(self.bn6(x))
-        x, mask = self.conv7(x, mask)
-        x = nn.ReLU(inplace=True)(self.bn7(x))
-        x, mask = self.conv8(x, mask)
-        x = nn.ReLU(inplace=True)(self.bn8(x))
-        x = nn.MaxPool2d(kernel_size=2, stride=2)(x)
-
-        x, mask = self.conv9(x, mask)
-        x = nn.ReLU(inplace=True)(self.bn9(x))
-        x, mask = self.conv10(x, mask)
-        x = nn.ReLU(inplace=True)(self.bn10(x))
-        x, mask = self.conv11(x, mask)
-        x = nn.ReLU(inplace=True)(self.bn11(x))
-        x, mask = self.conv12(x, mask)
-        x = nn.ReLU(inplace=True)(self.bn12(x))
-        x = nn.MaxPool2d(kernel_size=2, stride=2)(x)
-
-        x, mask = self.conv13(x, mask)
-        x = nn.ReLU(inplace=True)(self.bn13(x))
-        x, mask = self.conv14(x, mask)
-        x = nn.ReLU(inplace=True)(self.bn14(x))
-        x, mask = self.conv15(x, mask)
-        x = nn.ReLU(inplace=True)(self.bn15(x))
-        x, mask = self.conv16(x, mask)
-        x = nn.ReLU(inplace=True)(self.bn16(x))
-        x = nn.MaxPool2d(kernel_size=2, stride=2)(x)
-        # print("before linear")
-        x, mask = self.linear(x, mask)
+        # x = self.bn1(x)
+        # # print("x, after bn1", x.size())
+        # # print("before select: ", self.bn1_w.size(), mask.size())
+        # # print("after select: ", torch.masked_select(self.bn1_w, mask.squeeze()).size())
+        # masked_bn1_w = torch.masked_select(self.bn1_w, mask.squeeze()).view(1, mask.sum(), 1, 1)
+        # masked_bn1_b = torch.masked_select(self.bn1_b, mask.squeeze()).view(1, mask.sum(), 1, 1)
+        # # print("m_w, m_b, x", masked_bn1_w.size(), masked_bn1_b.size(), x.size())
+        # x = x*masked_bn1_w+masked_bn1_b
+        # # print("m_w, m_b, x", masked_bn1_w.size(), masked_bn1_b.size(), x.size())
+        # x = nn.ReLU(inplace=True)(x)
+        # x, mask = self.conv2(x, mask)
+        # # print("--3--")
+        #
+        # x = nn.ReLU(inplace=True)(self.bn2(x))
+        # x = nn.MaxPool2d(kernel_size=2, stride=2)(x)
+        #
+        # x, mask = self.conv3(x, mask)
+        # # print("--4--")
+        #
+        # x = nn.ReLU(inplace=True)(self.bn3(x))
+        # x, mask = self.conv4(x, mask)
+        # x = nn.ReLU(inplace=True)(self.bn4(x))
+        # x = nn.MaxPool2d(kernel_size=2, stride=2)(x)
+        #
+        # x, mask = self.conv5(x, mask)
+        # x = nn.ReLU(inplace=True)(self.bn5(x))
+        # x, mask = self.conv6(x, mask)
+        # x = nn.ReLU(inplace=True)(self.bn6(x))
+        # x, mask = self.conv7(x, mask)
+        # x = nn.ReLU(inplace=True)(self.bn7(x))
+        # x, mask = self.conv8(x, mask)
+        # x = nn.ReLU(inplace=True)(self.bn8(x))
+        # x = nn.MaxPool2d(kernel_size=2, stride=2)(x)
+        #
+        # x, mask = self.conv9(x, mask)
+        # x = nn.ReLU(inplace=True)(self.bn9(x))
+        # x, mask = self.conv10(x, mask)
+        # x = nn.ReLU(inplace=True)(self.bn10(x))
+        # x, mask = self.conv11(x, mask)
+        # x = nn.ReLU(inplace=True)(self.bn11(x))
+        # x, mask = self.conv12(x, mask)
+        # x = nn.ReLU(inplace=True)(self.bn12(x))
+        # x = nn.MaxPool2d(kernel_size=2, stride=2)(x)
+        #
+        # x, mask = self.conv13(x, mask)
+        # x = nn.ReLU(inplace=True)(self.bn13(x))
+        # x, mask = self.conv14(x, mask)
+        # x = nn.ReLU(inplace=True)(self.bn14(x))
+        # x, mask = self.conv15(x, mask)
+        # x = nn.ReLU(inplace=True)(self.bn15(x))
+        # x, mask = self.conv16(x, mask)
+        # x = nn.ReLU(inplace=True)(self.bn16(x))
+        # x = nn.MaxPool2d(kernel_size=2, stride=2)(x)
+        # # print("before linear")
+        # x, mask = self.linear(x, mask)
         # print("after linear")
         return x.squeeze()
 
