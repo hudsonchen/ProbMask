0bb2244cae1091f3d1ae1457ba9c73f41f0cda8a
x-zho14ls <18813119938@163.com>
2020-11-27 17:07:09 +0800

    a

--- args.py
+++ args.py
@@ -264,7 +264,7 @@ def parse_arguments():
 
     parser.add_argument(
         "--iterative",
-        default=True,
+        default=False,
         action="store_true",
         help="Whether use iterative pruning",
     )
