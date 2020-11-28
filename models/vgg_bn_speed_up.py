5a90aa29852ff4265348c7f464d26d64c301f396
x-zho14ls <18813119938@163.com>
2020-11-28 17:34:07 +0800

    Update vgg_bn_speed_up.py

--- models/vgg_bn_speed_up.py
+++ models/vgg_bn_speed_up.py
@@ -56,10 +56,10 @@ class VGG19(nn.Module):
         # print("x, after bn1", x.size())
         # print("before select: ", self.bn1_w.size(), mask.size())
         # print("after select: ", torch.masked_select(self.bn1_w, mask.squeeze()).size())
-        masked_bn1_w = torch.masked_select(self.bn1_w, mask.squeeze()).view(1, mask.sum(), 1, 1)
-        masked_bn1_b = torch.masked_select(self.bn1_b, mask.squeeze()).view(1, mask.sum(), 1, 1)
+        # masked_bn1_w = torch.masked_select(self.bn1_w, mask.squeeze()).view(1, mask.sum(), 1, 1)
+        # masked_bn1_b = torch.masked_select(self.bn1_b, mask.squeeze()).view(1, mask.sum(), 1, 1)
         # print("m_w, m_b, x", masked_bn1_w.size(), masked_bn1_b.size(), x.size())
-        x = x*masked_bn1_w+masked_bn1_b
+        # x = x*masked_bn1_w+masked_bn1_b
         # print("m_w, m_b, x", masked_bn1_w.size(), masked_bn1_b.size(), x.size())
         x = nn.ReLU(inplace=True)(x)
         x, mask = self.conv2(x, mask)
