3f43fd2609b20843bdff01eb2e2830dc685a78a0
x-zho14ls <18813119938@163.com>
2021-04-08 14:25:24 +0800

    Update conv_type.py

--- utils/conv_type.py
+++ utils/conv_type.py
@@ -126,6 +126,44 @@ class ProbMaskConvChannelDiscrete(nn.Conv2d):
             x = F.conv2d(x, w, self.bias, self.stride, self.padding, self.dilation, self.groups)
         return x
 
+class ProbMaskConvChannelDiscrete(nn.Conv2d):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.scores = nn.Parameter(torch.Tensor(self.weight.size()[0], 1, 1, 1))
+        self.subnet = None
+        self.train_weights = False
+        self.rescaling_para = nn.Parameter(torch.Tensor(1))
+        if parser_args.score_init_constant is not None:
+            self.scores.data = (
+                    torch.ones_like(self.scores) * parser_args.score_init_constant
+            )
+        else:
+            nn.init.kaiming_uniform_(self.scores, a=math.sqrt(5))
+
+    @property
+    def clamped_scores(self):
+        return self.scores
+
+    def fix_subnet(self):
+        self.subnet = (torch.rand_like(self.scores) < self.clamped_scores).float()
+
+    def forward(self, x):
+        if not self.train_weights:
+            eps = 1e-20
+            temp = parser_args.T
+            uniform0 = torch.rand_like(self.scores)
+            uniform1 = torch.rand_like(self.scores)
+            noise = -torch.log(torch.log(uniform0 + eps) / torch.log(uniform1 + eps) + eps)
+            self.subnet = GetMaskDiscrete.apply(torch.sigmoid((torch.log(self.clamped_scores + eps) - torch.log(1.0 - self.clamped_scores + eps) + noise) * temp))
+            self.subnet = GetMaskDiscreteBS.apply(self.scores)
+            # print(self.subnet.size(), self.weight.size(), self.subnet)
+            w = self.weight * self.subnet
+            x = F.conv2d(x, w, self.bias, self.stride, self.padding, self.dilation, self.groups)
+        else:
+            w = self.weight * self.subnet
+            x = F.conv2d(x, w, self.bias, self.stride, self.padding, self.dilation, self.groups)
+        return x
+
 
 class ProbMaskConvChannelDiscreteSpeedUp(nn.Conv2d):
     def __init__(self, *args, **kwargs):
@@ -196,6 +234,70 @@ class ProbMaskConvChannelDiscreteSpeedUp(nn.Conv2d):
             x = F.conv2d(x, w, self.bias, self.stride, self.padding, self.dilation, self.groups)
         return x, self.subnet
 
+class GrowEfficientConvProbReal(nn.Conv2d):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.scores = nn.Parameter(torch.Tensor(self.weight.size()[0], 1, 1, 1))
+        self.subnet = None
+        self.train_weights = False
+        if parser_args.score_init_constant is not None:
+            self.scores.data = (
+                    torch.ones_like(self.scores) * parser_args.score_init_constant
+            )
+        else:
+            nn.init.kaiming_uniform_(self.scores, a=math.sqrt(5))
+        if self.out_channels == 10 or self.out_channels == 100:
+            self.prune = False
+            self.subnet = torch.ones_like(self.scores).cuda(parser_args.multigpu[0])
+        else:
+            self.prune = True
+        self.rescaling_para = nn.Parameter(torch.Tensor(1))
+        self.rescaling_para.data = torch.mean(self.clamped_scores).data
+
+    @property
+    def clamped_scores(self):
+        return self.scores
+
+    def fix_subnet(self):
+        self.subnet = self.scores*GetMaskDiscreteBSReal.apply(self.scores)
+
+    def forward(self, x):
+        if self.prune:
+            if not self.train_weights:
+                self.subnet = self.scores*GetMaskDiscreteBSReal.apply(self.scores)
+                w = self.weight * self.subnet
+                x = F.conv2d(x, w, self.bias, self.stride, self.padding, self.dilation, self.groups)
+            else:
+                w = self.weight * self.subnet
+                x = F.conv2d(x, w, self.bias, self.stride, self.padding, self.dilation, self.groups)
+        else:
+            x = F.conv2d(x, self.weight, self.bias, self.stride, self.padding, self.dilation, self.groups)
+        return x
+
+
+class GetMaskDiscreteBS(autograd.Function):
+    @staticmethod
+    def forward(ctx, m_cont):
+        output = (torch.rand_like(m_cont) <= m_cont).float()
+        return output
+
+    @staticmethod
+    def backward(ctx, grad_outputs):
+        return grad_outputs
+
+class GetMaskDiscreteBSReal(autograd.Function):
+    @staticmethod
+    def forward(ctx, m_cont):
+        output = (torch.rand_like(m_cont) <= m_cont).float()
+        ctx.save_for_backward(output)
+        return output
+
+    @staticmethod
+    def backward(ctx, grad_outputs):
+        m_dis, = ctx.saved_variables
+        grad_inputs = grad_outputs.clone()
+        grad_inputs[m_dis == 0.0] = 0.0
+        return grad_inputs
 
 class GetMaskDiscrete(autograd.Function):
     @staticmethod
